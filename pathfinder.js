ee4b0d0de751bc3243d1cb2ab2006e211567e757
Mixailka MKSK <mk06ru@gmail.com>
2025-07-20 19:25:24 +0300

    deploy: Build from commit main@5ae61d5

--- pathfinder.js
+++ pathfinder.js
@@ -4,7 +4,7 @@ const cachedTextDecoder = (typeof TextDecoder !== 'undefined' ? new TextDecoder(
 
 if (typeof TextDecoder !== 'undefined') { cachedTextDecoder.decode(); };
 
-let cachedUint8ArrayMemory0 = null; // s
+let cachedUint8ArrayMemory0 = null;
 
 function getUint8ArrayMemory0() {
     if (cachedUint8ArrayMemory0 === null || cachedUint8ArrayMemory0.byteLength === 0) {
@@ -50,125 +50,26 @@ function getArrayI32FromWasm0(ptr, len) {
     return getInt32ArrayMemory0().subarray(ptr / 4, ptr / 4 + len);
 }
 /**
- * @param {Int32Array} data
- * @returns {Int32Array}
- */
-export function find_path_on_grid_wasm(data) {
-    const ptr0 = passArray32ToWasm0(data, wasm.__wbindgen_malloc);
-    const len0 = WASM_VECTOR_LEN;
-    const ret = wasm.find_path_on_grid_wasm(ptr0, len0);
-    var v2 = getArrayI32FromWasm0(ret[0], ret[1]).slice();
-    wasm.__wbindgen_free(ret[0], ret[1] * 4, 4);
-    return v2;
-}
-
-function _assertClass(instance, klass) {
-    if (!(instance instanceof klass)) {
-        throw new Error(`expected instance of ${klass.name}`);
-    }
-}
-/**
  * @param {number} start_x
  * @param {number} start_y
  * @param {number} goal_x
  * @param {number} goal_y
- * @param {Int32Array} obstacles_flat
- * @param {PhysicsParams} params
- * @param {number} grid_size
+ * @param {Int32Array} costs_flat
+ * @param {Int32Array} zone_types_flat
+ * @param {Int32Array} teleporters_flat
  * @returns {Int32Array}
  */
-export function find_path_in_space_wasm(start_x, start_y, goal_x, goal_y, obstacles_flat, params, grid_size) {
-    const ptr0 = passArray32ToWasm0(obstacles_flat, wasm.__wbindgen_malloc);
+export function find_path_on_grid_wasm(start_x, start_y, goal_x, goal_y, costs_flat, zone_types_flat, teleporters_flat) {
+    const ptr0 = passArray32ToWasm0(costs_flat, wasm.__wbindgen_malloc);
     const len0 = WASM_VECTOR_LEN;
-    _assertClass(params, PhysicsParams);
-    const ret = wasm.find_path_in_space_wasm(start_x, start_y, goal_x, goal_y, ptr0, len0, params.__wbg_ptr, grid_size);
-    var v2 = getArrayI32FromWasm0(ret[0], ret[1]).slice();
+    const ptr1 = passArray32ToWasm0(zone_types_flat, wasm.__wbindgen_malloc);
+    const len1 = WASM_VECTOR_LEN;
+    const ptr2 = passArray32ToWasm0(teleporters_flat, wasm.__wbindgen_malloc);
+    const len2 = WASM_VECTOR_LEN;
+    const ret = wasm.find_path_on_grid_wasm(start_x, start_y, goal_x, goal_y, ptr0, len0, ptr1, len1, ptr2, len2);
+    var v4 = getArrayI32FromWasm0(ret[0], ret[1]).slice();
     wasm.__wbindgen_free(ret[0], ret[1] * 4, 4);
-    return v2;
-}
-
-const PhysicsParamsFinalization = (typeof FinalizationRegistry === 'undefined')
-    ? { register: () => {}, unregister: () => {} }
-    : new FinalizationRegistry(ptr => wasm.__wbg_physicsparams_free(ptr >>> 0, 1));
-
-export class PhysicsParams {
-
-    __destroy_into_raw() {
-        const ptr = this.__wbg_ptr;
-        this.__wbg_ptr = 0;
-        PhysicsParamsFinalization.unregister(this);
-        return ptr;
-    }
-
-    free() {
-        const ptr = this.__destroy_into_raw();
-        wasm.__wbg_physicsparams_free(ptr, 0);
-    }
-    /**
-     * @returns {number}
-     */
-    get gravity_y() {
-        const ret = wasm.__wbg_get_physicsparams_gravity_y(this.__wbg_ptr);
-        return ret;
-    }
-    /**
-     * @param {number} arg0
-     */
-    set gravity_y(arg0) {
-        wasm.__wbg_set_physicsparams_gravity_y(this.__wbg_ptr, arg0);
-    }
-    /**
-     * @returns {number}
-     */
-    get jump_impulse_y() {
-        const ret = wasm.__wbg_get_physicsparams_jump_impulse_y(this.__wbg_ptr);
-        return ret;
-    }
-    /**
-     * @param {number} arg0
-     */
-    set jump_impulse_y(arg0) {
-        wasm.__wbg_set_physicsparams_jump_impulse_y(this.__wbg_ptr, arg0);
-    }
-    /**
-     * @returns {number}
-     */
-    get max_velocity_y() {
-        const ret = wasm.__wbg_get_physicsparams_max_velocity_y(this.__wbg_ptr);
-        return ret;
-    }
-    /**
-     * @param {number} arg0
-     */
-    set max_velocity_y(arg0) {
-        wasm.__wbg_set_physicsparams_max_velocity_y(this.__wbg_ptr, arg0);
-    }
-    /**
-     * @returns {number}
-     */
-    get time_step_ms() {
-        const ret = wasm.__wbg_get_physicsparams_time_step_ms(this.__wbg_ptr);
-        return ret;
-    }
-    /**
-     * @param {number} arg0
-     */
-    set time_step_ms(arg0) {
-        wasm.__wbg_set_physicsparams_time_step_ms(this.__wbg_ptr, arg0);
-    }
-    /**
-     * @returns {number}
-     */
-    get velocity_granularity() {
-        const ret = wasm.__wbg_get_physicsparams_velocity_granularity(this.__wbg_ptr);
-        return ret;
-    }
-    /**
-     * @param {number} arg0
-     */
-    set velocity_granularity(arg0) {
-        wasm.__wbg_set_physicsparams_velocity_granularity(this.__wbg_ptr, arg0);
-    }
+    return v4;
 }
 
 async function __wbg_load(module, imports) {
@@ -205,9 +106,6 @@ async function __wbg_load(module, imports) {
 function __wbg_get_imports() {
     const imports = {};
     imports.wbg = {};
-    imports.wbg.__wbg_log_c222819a41e063d3 = function(arg0) {
-        console.log(arg0);
-    };
     imports.wbg.__wbindgen_init_externref_table = function() {
         const table = wasm.__wbindgen_export_0;
         const offset = table.grow(4);
@@ -217,10 +115,6 @@ function __wbg_get_imports() {
         table.set(offset + 2, true);
         table.set(offset + 3, false);
         ;
-    };
-    imports.wbg.__wbindgen_string_new = function(arg0, arg1) {
-        const ret = getStringFromWasm0(arg0, arg1);
-        return ret;
     };
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
