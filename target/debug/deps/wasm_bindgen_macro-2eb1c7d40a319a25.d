bf44ec8b5c7e54e34c4360b093972be547b63fa5
Mixailka MKSK <mk06ru@gmail.com>
2025-07-20 18:55:49 +0300

    update

---
+++ target/debug/deps/wasm_bindgen_macro-2eb1c7d40a319a25.d
@@ -0,0 +1,5 @@
+X:\RustIDE\projects\pathfinder-wasm\target\debug\deps\wasm_bindgen_macro-2eb1c7d40a319a25.d: C:\Users\USER\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-0.2.100\src\lib.rs
+
+X:\RustIDE\projects\pathfinder-wasm\target\debug\deps\wasm_bindgen_macro-2eb1c7d40a319a25.dll: C:\Users\USER\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-0.2.100\src\lib.rs
+
+C:\Users\USER\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-0.2.100\src\lib.rs:
---
+++ target/debug/deps/wasm_bindgen_shared-84a21b496be901b7.d
@@ -0,0 +1,11 @@
+X:\RustIDE\projects\pathfinder-wasm\target\debug\deps\wasm_bindgen_shared-84a21b496be901b7.d: C:\Users\USER\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-shared-0.2.100\src\lib.rs C:\Users\USER\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-shared-0.2.100\src\identifier.rs
+
+X:\RustIDE\projects\pathfinder-wasm\target\debug\deps\libwasm_bindgen_shared-84a21b496be901b7.rlib: C:\Users\USER\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-shared-0.2.100\src\lib.rs C:\Users\USER\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-shared-0.2.100\src\identifier.rs
+
+X:\RustIDE\projects\pathfinder-wasm\target\debug\deps\libwasm_bindgen_shared-84a21b496be901b7.rmeta: C:\Users\USER\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-shared-0.2.100\src\lib.rs C:\Users\USER\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-shared-0.2.100\src\identifier.rs
+
+C:\Users\USER\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-shared-0.2.100\src\lib.rs:
+C:\Users\USER\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-shared-0.2.100\src\identifier.rs:
+
+# env-dep:CARGO_PKG_VERSION=0.2.100
+# env-dep:WBG_VERSION
